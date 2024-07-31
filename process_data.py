df4c695cadea8af4a9b2fbc9b854b904dbf16805
SofiaMartinez <Sofijm6074@gmail.com>
2024-07-31 19:32:32 -0300

    Identificación de bots y crawlers

--- process_data.py
+++ process_data.py
@@ -1,9 +1,32 @@
 import polars as pl;
 
-df = pl.read_csv('archivo.csv')
-df_filtrado = df.filter(pl.col("status_code") == 200)
-df_cleaned = df_filtrado.with_columns([
-    pl.col("timestamp").str.strptime(pl.Datetime, "%d/%b/%Y:%H:%M:%S %z", strict=False).alias("timestamp")
-])
+# - - - - - FUNCIONES AUXILIARES
+def filtrar_googlebot(df):
+    return df.filter(~pl.col("user_agent").str.contains('Googlebot'))
 
-print(df_cleaned)
+def filtrar_baiduspider(df):
+    return df.filter(~pl.col("user_agent").str.contains('Baiduspider'))
+
+def filtrar_agesic_crawler(df):
+    return df.filter(~pl.col("user_agent").str.contains('agesic-crawler'))
+
+def filtrar_robots_y_crawlers(df):
+    df = filtrar_googlebot(df)
+    df = filtrar_baiduspider(df)
+    df = filtrar_agesic_crawler(df)
+    return df
+
+def formatear_fecha(df):
+    return df.with_columns([pl.col("timestamp").str.strptime(pl.Datetime, "%d/%b/%Y:%H:%M:%S %z", strict=False).alias("timestamp")])
+
+# - - - - - MAIN
+df = pl.read_csv('archivo.csv')
+df_filtrado = filtrar_robots_y_crawlers(df)
+df_cleaned = formatear_fecha(df_filtrado)
+print(df_cleaned.select(pl.col(["user_agent"]))) # .glimpse() para ver string completo
+
+
+# TO-DO:
+# • Detección de páginas más visitadas.
+# • Análisis de navegación de los usuarios.
+# • Identificación de errores en respuestas a solicitudes de páginas web.
