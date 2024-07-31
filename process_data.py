093e75f114dc7949f5ef949e6634cb733d09c7e6
SofiaMartinez <Sofijm6074@gmail.com>
2024-07-31 19:54:07 -0300

    Detección de páginas más visitadas

--- process_data.py
+++ process_data.py
@@ -16,17 +16,28 @@ def filtrar_robots_y_crawlers(df):
     df = filtrar_agesic_crawler(df)
     return df
 
+def filtrar_URLs_vacias(df):
+    return df.filter(pl.col("url").is_not_null())
+
+def count_frecuencia_url(df):
+    return df.group_by("url").agg(pl.col("url").count().alias("visit_count"))
+
 def formatear_fecha(df):
     return df.with_columns([pl.col("timestamp").str.strptime(pl.Datetime, "%d/%b/%Y:%H:%M:%S %z", strict=False).alias("timestamp")])
 
 # - - - - - MAIN
 df = pl.read_csv('archivo.csv')
+
+# Limpiando bots y crawlers
 df_filtrado = filtrar_robots_y_crawlers(df)
 df_cleaned = formatear_fecha(df_filtrado)
-print(df_cleaned.select(pl.col(["user_agent"]))) # .glimpse() para ver string completo
+print(df_cleaned) # .glimpse() para ver string completo
+
+# Páginas más visitadas
+df_frecuencia_url_ordenado = count_frecuencia_url(filtrar_URLs_vacias(df)).sort("visit_count")
+print(df_frecuencia_url_ordenado)
 
 
 # TO-DO:
-# • Detección de páginas más visitadas.
 # • Análisis de navegación de los usuarios.
 # • Identificación de errores en respuestas a solicitudes de páginas web.
