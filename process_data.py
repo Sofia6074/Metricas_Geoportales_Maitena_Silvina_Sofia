2d970cd906822e4a3d3cb149c3d233670d3eb3b4
SofiaMartinez <Sofijm6074@gmail.com>
2024-08-04 12:23:57 -0300

    Se añade .ipynb con nuevas funciones auxiliares de limpieza

--- process_data.py
+++ process_data.py
@@ -16,6 +16,16 @@ def filtrar_robots_y_crawlers(df):
     df = filtrar_agesic_crawler(df)
     return df
 
+def filtrar_map_server(df):
+    # MapServer es una plataforma de código abierto para publicar datos espaciales y aplicaciones interactivas de mapeo en la web.
+    # libcurl es una biblioteca utilizada por MapServer para manejar transferencias de datos a través de varios protocolos de red.
+    return df.filter(~pl.col("url").str.contains('/wfsPCN1000.cgi'))
+
+def filtrar_datos(df):
+    df = filtrar_robots_y_crawlers(df)
+    df = filtrar_map_server(df)
+    return df
+
 def filtrar_URLs_vacias(df):
     return df.filter(pl.col("url").is_not_null())
 
@@ -28,13 +38,13 @@ def formatear_fecha(df):
 # - - - - - MAIN
 df = pl.read_csv('archivo.csv')
 
-# Limpiando bots y crawlers
-df_filtrado = filtrar_robots_y_crawlers(df)
+# Limpieza de datos
+df_filtrado = filtrar_datos(df)
 df_cleaned = formatear_fecha(df_filtrado)
 print(df_cleaned) # .glimpse() para ver string completo
 
 # Páginas más visitadas
-df_frecuencia_url_ordenado = count_frecuencia_url(filtrar_URLs_vacias(df)).sort("visit_count")
+df_frecuencia_url_ordenado = count_frecuencia_url(filtrar_URLs_vacias(df_cleaned)).sort("visit_count")
 print(df_frecuencia_url_ordenado)
 
 
