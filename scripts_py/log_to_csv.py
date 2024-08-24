1888addc87fcd637c84345384ba208959ea3a1ab
SofiaMartinez <Sofijm6074@gmail.com>
2024-08-24 16:04:28 -0300

    feat: [issue-13] formateando para pasar validaciones

--- scripts_py/log_to_csv.py
+++ scripts_py/log_to_csv.py
@@ -1,5 +1,5 @@
-
 import csv
+
 
 def convert_log_to_csv(log_file_path, csv_file_path):
     # Load the log data
@@ -7,7 +7,10 @@ def convert_log_to_csv(log_file_path, csv_file_path):
         log_data = file.readlines()
 
     # Define headers for the CSV
-    headers = ["ip", "timestamp", "request_method", "url", "http_version", "status_code", "size", "user_agent"]
+    headers = [
+        "ip", "timestamp", "request_method", "url",
+        "http_version", "status_code", "size", "user_agent"
+    ]
 
     # Write log data to CSV
     with open(csv_file_path, 'w', newline='') as csvfile:
@@ -20,15 +23,30 @@ def convert_log_to_csv(log_file_path, csv_file_path):
             try:
                 ip = parts[0]
                 timestamp = ' '.join(parts[3:5]).strip('[]')
-                request_method = parts[5].strip('"') if parts[5] != '"-"' else None
+                request_method = (parts[5].strip('"')
+                                  if parts[5] != '"-"' else None)
                 url = parts[6] if len(parts) > 6 else None
                 http_version = parts[7].strip('"') if len(parts) > 7 else None
-                status_code = int(parts[8]) if len(parts) > 8 and parts[8].isdigit() else None
-                size = int(parts[9]) if len(parts) > 9 and parts[9].isdigit() else None
-                user_agent = ' '.join(parts[11:]).strip('"') if len(parts) > 11 else None
+                status_code = (
+                    int(parts[8]) if len(parts) > 8
+                    and parts[8].isdigit() else None
+                )
+                size = (
+                    int(parts[9]) if len(parts) > 9
+                    and parts[9].isdigit() else None
+                )
+                user_agent = (
+                    ' '.join(parts[11:]).strip('"')
+                    if len(parts) > 11 else None
+                )
 
-                csvwriter.writerow([ip, timestamp, request_method, url, http_version, status_code, size, user_agent])
+                csvwriter.writerow([
+                    ip, timestamp, request_method, url, 
+                    http_version, status_code, size, user_agent
+                ])
 
             except (IndexError, ValueError) as e:
                 print(f"Error processing line: {line.strip()} - Error: {e}")
-                csvwriter.writerow([ip, timestamp, None, None, None, None, None, None])
+                csvwriter.writerow([ip, timestamp,
+                                    None, None, None, None, None, None])
+
--- scripts_py/normalize_data.py
+++ scripts_py/normalize_data.py
@@ -2,7 +2,12 @@ import polars as pl
 
 
 def formatear_fecha(df):
-    return df.with_columns([pl.col("timestamp").str.strptime(pl.Datetime, "%d/%b/%Y:%H:%M:%S %z", strict=False).dt.strftime("%d-%m-%Y %H:%M:%S").alias("timestamp")])
+    return df.with_columns([pl.col("timestamp")
+                            .str.strptime(pl.Datetime, "%d/%b/%Y:%H:%M:%S %z",
+                                          strict=False).dt.strftime(
+                                              "%d-%m-%Y %H:%M:%S").alias(
+                                                  "timestamp")])
+
 
 def normalizar_datos(df):
     return formatear_fecha(df)
