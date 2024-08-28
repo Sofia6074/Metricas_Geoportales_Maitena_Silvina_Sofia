7e8d50b1ee1e4e6ae6b9a7de2a19d97374e2c738
Maite <80791547+Mai-Ri@users.noreply.github.com>
2024-08-28 20:00:37 -0300

    fix: [issue-4] fix lint errors

--- metrics/global/error_rate_success_rate.py
+++ metrics/global/error_rate_success_rate.py
@@ -1,27 +1,20 @@
 import polars as pl
-from datetime import timedelta
 
 
 def main():
-    # Cargar el archivo CSV
     df = pl.read_csv("/Users/admin/Documents/TesisArchivo/parsed_logs_with_headers.csv")
 
-    # Asumiendo que el status_code está en la columna "status_code"
-    # Ajusta el nombre de la columna según sea necesario
     df = df.with_columns(pl.col("status_code").cast(pl.Int32))
 
-    # Filtrar para obtener solo las filas con códigos de estado de éxito y error
     success_df = df.filter(
         (pl.col("status_code") >= 200) & (pl.col("status_code") < 300)
     )
     error_df = df.filter((pl.col("status_code") >= 400) & (pl.col("status_code") < 600))
 
-    # Calcular los totales
     total_requests = df.shape[0]
     success_count = success_df.shape[0]
     error_count = error_df.shape[0]
 
-    # Calcular las tasas
     success_rate = (success_count / total_requests) * 100
     error_rate = (error_count / total_requests) * 100
 
