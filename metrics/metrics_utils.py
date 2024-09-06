dc766a4bd8766d83c0d03477206046897c07e85a
SofiaMartinez <Sofijm6074@gmail.com>
2024-09-06 15:15:17 -0300

    [issue-14] convert metric from pyspark to polars

--- metrics/metrics_utils.py
+++ metrics/metrics_utils.py
@@ -2,36 +2,36 @@
 Este módulo contiene utilidades para el cálculo de sesiones en los logs de mapas.
 """
 
-from pyspark.sql import Window
-from pyspark.sql.functions import unix_timestamp, col, when, count, expr, lag
+import polars as pl
 
 def calculate_sessions(map_requests_df):
     """
     Calcula las sesiones únicas basadas en la IP y el agente de usuario.
     """
-    map_requests_df = map_requests_df.withColumn(
-        "session_id", col("ip") + "_" + col("user_agent")
-    )
 
-    window_spec = Window.partitionBy("session_id").orderBy(
-        unix_timestamp("timestamp", "dd/MMM/yyyy:HH:mm:ss Z")
-    )
+    map_requests_df = map_requests_df.with_columns([
+        (pl.col("ip") + "_" + pl.col("user_agent")).alias("session_id")
+    ])
 
-    map_requests_df = map_requests_df.withColumn(
-        "prev_timestamp", lag("timestamp").over(window_spec)
-    )
-    map_requests_df = map_requests_df.withColumn(
-        "time_diff",
-        unix_timestamp("timestamp", "dd/MMM/yyyy:HH:mm:ss Z") -
-        unix_timestamp("prev_timestamp", "dd/MMM/yyyy:HH:mm:ss Z")
-    )
-    map_requests_df = map_requests_df.withColumn(
-        "new_session", when(col("time_diff") > 1800, 1).otherwise(0)
-    )
-    map_requests_df = map_requests_df.withColumn(
-        "session_number",
-        count("new_session").over(window_spec.rowsBetween(Window.unboundedPreceding, 0))
-    )
-    return map_requests_df.withColumn(
-        "unique_session_id", expr("concat(ip, '_', user_agent, '_', session_number)")
-    )
+    map_requests_df = map_requests_df.with_columns([
+        pl.col("timestamp").shift(1).over("session_id").alias("prev_timestamp")
+    ])
+
+    map_requests_df = map_requests_df.with_columns([
+        (pl.col("timestamp") - pl.col("prev_timestamp")).alias("time_diff")
+    ])
+
+    # Si el tiempo entre dos timestamps es mayor a 1800 segundos (30 minutos), es una nueva sesión
+    map_requests_df = map_requests_df.with_columns([
+        pl.when(pl.col("time_diff") > 1800).then(1).otherwise(0).alias("new_session")
+    ])
+
+    map_requests_df = map_requests_df.with_columns([
+        pl.col("new_session").cum_sum().over("session_id").alias("session_number")
+    ])
+
+    map_requests_df = map_requests_df.with_columns([
+        (pl.col("ip") + "_" + pl.col("user_agent") + "_" + pl.col("session_number").cast(pl.Utf8)).alias("unique_session_id")
+    ])
+
+    return map_requests_df
