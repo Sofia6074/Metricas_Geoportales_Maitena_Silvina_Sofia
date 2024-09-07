765729d00cd01dc187b314db8aa5b4aa20147605
SofiaMartinez <Sofijm6074@gmail.com>
2024-09-07 12:08:26 -0300

    [issue-20] ordenando sesiones en calculate_sessions

--- metrics/metrics_utils.py
+++ metrics/metrics_utils.py
@@ -13,6 +13,8 @@ def calculate_sessions(map_requests_df):
         (pl.col("ip") + "_" + pl.col("user_agent")).alias("session_id")
     ])
 
+    map_requests_df = map_requests_df.sort(by=["session_id", "timestamp"])
+
     map_requests_df = map_requests_df.with_columns([
         pl.col("timestamp").shift(1).over("session_id").alias("prev_timestamp")
     ])
@@ -31,7 +33,8 @@ def calculate_sessions(map_requests_df):
     ])
 
     map_requests_df = map_requests_df.with_columns([
-        (pl.col("ip") + "_" + pl.col("user_agent") + "_" + pl.col("session_number").cast(pl.Utf8)).alias("unique_session_id")
+        (pl.col("ip") + "_" + pl.col("user_agent") + "_" + pl.col("session_number").cast(pl.Utf8)).alias(
+            "unique_session_id")
     ])
 
     return map_requests_df
