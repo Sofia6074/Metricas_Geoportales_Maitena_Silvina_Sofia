1dfefcd304ef31e3b7814e0e728fda3931da78d1
SofiaMartinez <Sofijm6074@gmail.com>
2024-09-07 22:17:06 -0300

    [issue-33] Improving Logger class

--- scripts_py/classes/logger.py
+++ scripts_py/classes/logger.py
@@ -4,7 +4,10 @@ import logging
 class Logger:
     def __init__(self, name: str):
         self.logger = logging.getLogger(name)
-        logging.basicConfig(level=logging.INFO)
+        logging.basicConfig(
+            level=logging.INFO,
+            format='(%(name)s): %(message)s'
+        )
 
     def get_logger(self):
         return self.logger
