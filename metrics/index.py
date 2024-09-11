c31a68eafaaff5556877c2a55f51a83f1898ce32
SofiaMartinez <Sofijm6074@gmail.com>
2024-09-10 21:29:17 -0300

    [issue-71] calculating device usages

--- metrics/index.py
+++ metrics/index.py
@@ -2,13 +2,20 @@
 This module runs all metrics using a DataFrame of logs.
 """
 
-from metrics.general.average_response_time import calculate_average_response_time
-from metrics.general.average_time_spent_on_site import calculate_average_time_spent_on_site
-from metrics.general.average_time_spent_per_page import calculate_average_time_spent_per_page
-from metrics.general.error_rate_success_rate import calculate_error_rate_success_rate
-from metrics.maps.maximum_stable_value_zoom import calculate_maximum_stable_value_zoom
+from metrics.general.average_response_time import (
+    calculate_average_response_time)
+from metrics.general.average_time_spent_on_site import (
+    calculate_average_time_spent_on_site)
+from metrics.general.average_time_spent_per_page import (
+    calculate_average_time_spent_per_page)
+from metrics.general.count_device_usage import count_device_usage
+from metrics.general.error_rate_success_rate import (
+    calculate_error_rate_success_rate)
+from metrics.maps.maximum_stable_value_zoom import (
+    calculate_maximum_stable_value_zoom)
 from metrics.nav.most_visited_pages import calculate_nav_most_visited_pages
-from metrics.user.average_stepback_actions import calculate_average_stepback_actions
+from metrics.user.average_stepback_actions import (
+    calculate_average_stepback_actions)
 from metrics.users.average_pages_viewed_per_visitor import (
     calculate_average_pages_viewed_per_session)
 from metrics.users.ratio_of_new_visitors_to_all_visitors import (
@@ -27,6 +34,7 @@ def run_all_metrics(logs_df):
     calculate_average_time_spent_per_page(logs_df)
     calculate_average_response_time(logs_df)
     calculate_average_time_spent_on_site(logs_df)
+    count_device_usage(logs_df)
 
     print("Time-related metrics:")
     calculate_maximum_stable_value_zoom(logs_df)
