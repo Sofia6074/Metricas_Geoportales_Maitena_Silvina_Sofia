f0dc79665ef961dbb0b32417f961410396ea104c
SofiaMartinez <Sofijm6074@gmail.com>
2024-09-25 20:46:36 -0300

    [issue-84] Deleting unused imports

--- frontend/src/app/overview/page.tsx
+++ frontend/src/app/overview/page.tsx
@@ -4,56 +4,12 @@ import { useContext, useEffect } from "react";
 import Breadcrumb from "@/components/breadcrumb/breadcrumb";
 import styles from "./overview.module.css"
 import Card from "@/components/card/card";
-import { LineChart, Line, ResponsiveContainer, Bar, BarChart, PieChart, Pie } from 'recharts';
+import { ResponsiveContainer, PieChart, Pie } from 'recharts';
 import { MetricsContext } from "@/context/MetricsContext";
 import Spinner from "@/components/spinner/spinner";
 import AutoSizeText from "@/components/autosizeText/autosizeText";
 
 export default function Overview() {
-    const data = [
-        {
-            name: 'Page A',
-            uv: 4000,
-            pv: 2400,
-            amt: 2400,
-        },
-        {
-            name: 'Page B',
-            uv: 3000,
-            pv: 1398,
-            amt: 2210,
-        },
-        {
-            name: 'Page C',
-            uv: 2000,
-            pv: 9800,
-            amt: 2290,
-        },
-        {
-            name: 'Page D',
-            uv: 2780,
-            pv: 3908,
-            amt: 2000,
-        },
-        {
-            name: 'Page E',
-            uv: 1890,
-            pv: 4800,
-            amt: 2181,
-        },
-        {
-            name: 'Page F',
-            uv: 2390,
-            pv: 3800,
-            amt: 2500,
-        },
-        {
-            name: 'Page G',
-            uv: 3490,
-            pv: 4300,
-            amt: 2100,
-        },
-    ];
     const { metrics, loading, error } = useContext(MetricsContext);
 
     useEffect(() => {
