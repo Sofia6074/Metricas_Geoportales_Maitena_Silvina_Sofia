16a071c4d3613584962e283835309b5040b5791a
SofiaMartinez <Sofijm6074@gmail.com>
2024-09-27 23:02:10 -0300

    [issue-93] Adding missing metrics to json

--- metrics/index.py
+++ metrics/index.py
@@ -36,6 +36,10 @@ from metrics.users.average_stepback_actions import (
 from metrics.users.ratio_of_new_visitors_to_all_visitors import (
     calculate_ratio_of_new_visitors_to_all_visitors
 )
+from metrics.users.user_categorization.average_pages_viewed_per_user_cat import \
+    calculate_average_pages_viewed_per_session_by_profile
+from metrics.users.user_categorization.average_time_spent_on_site_per_user_cat import calculate_weighted_average_time_spent_on_site
+from metrics.users.user_categorization.average_time_spent_per_page_per_user_cat import calculate_average_time_spent_per_page_per_user_cat
 from metrics.users.user_categorization.index import (
     classify_user_profiles
 )
@@ -71,9 +75,13 @@ def run_all_metrics(logs_df):
 
     print("User metrics:")
     results['average_pages_viewed'] = calculate_average_pages_viewed_per_session(logs_df)
+    results['average_pages_viewed_categorization'] = calculate_average_pages_viewed_per_session_by_profile(logs_df)
     results['new_visitors_vs_all_visitors'] = calculate_ratio_of_new_visitors_to_all_visitors(logs_df)
     results['avrg_stepbacks'] = calculate_average_stepback_actions(logs_df)
     results['user_profiles'] = classify_user_profiles(logs_df)
+    results['average_time_spent_on_site_categorization'] = calculate_weighted_average_time_spent_on_site(logs_df)
+    results['average_time_spent_per_page_categorization'] = calculate_average_time_spent_per_page_per_user_cat(logs_df)
+
 
     print("All metrics have been executed.")
     create_json(results)
