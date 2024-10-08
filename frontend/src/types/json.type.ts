a82e20a7628de5de99bdd9b82a68e859edcec231
SofiaMartinez <Sofijm6074@gmail.com>
2024-10-08 20:23:28 -0300

    [issue-20] Fixing calculate_jaccard_similarity

--- frontend/src/types/json.type.ts
+++ frontend/src/types/json.type.ts
@@ -1,20 +1,22 @@
-export interface JsonData {
+export interface Root {
     error_rate_success_rate: ErrorRateSuccessRate
     average_time_spent_on_site: number
     average_time_spent_per_page: number
     average_response_time: AverageResponseTime
     device_usage: DeviceUsage[]
-    downloadable_resources_hits_ratio: number
+    downloadable_resources_hits: number
     stick_and_slip_pages: StickAndSlipPages
+    count_device_usage: CountDeviceUsage[]
+    most_visited_pages: any
     maximum_zoom: number
     maximum_stable_zoom: MaximumStableZoom[]
     zoom_response_time: number
     most_repeated_words: MostRepeatedWord[]
-    related_search_parameters: RelatedSearchParameter[]
+    related_search_parameters: any[]
     average_pages_viewed: number
     new_visitors_vs_all_visitors: number
-    avrg_stepbacks: number
-    user_profiles: UserProfile[]
+    average_stepbacks: number
+    user_profiles: UserProfiles
 }
 
 export interface ErrorRateSuccessRate {
@@ -40,6 +42,11 @@ export interface StickAndSlipPages {
     stick: number
 }
 
+export interface CountDeviceUsage {
+    device_type: string
+    device_usage_count: number
+}
+
 export interface MaximumStableZoom {
     zoom_level: number
     total_count: number
@@ -50,14 +57,34 @@ export interface MostRepeatedWord {
     count: number
 }
 
-export interface RelatedSearchParameter {
-    unique_session_id: string
-    search_pair: string
-    jaccard_similarity: number
+export interface UserProfiles {
+    user_profile_counts: UserProfileCount[]
+    user_categorized_metrics: UserCategorizedMetrics
 }
 
-export interface UserProfile {
+export interface UserProfileCount {
     user_profile: number
     count: number
     percentage: number
+}
+
+export interface UserCategorizedMetrics {
+    average_time_spent_on_site_per_user_category: AverageTimeSpentOnSitePerUserCategory[]
+    average_pages_viewed_per_session_per_user_category: AveragePagesViewedPerSessionPerUserCategory[]
+    average_time_spent_per_page_per_user_category: AverageTimeSpentPerPagePerUserCategory[]
+}
+
+export interface AverageTimeSpentOnSitePerUserCategory {
+    user_profile: number
+    avg_time_spent_on_site: number
+}
+
+export interface AveragePagesViewedPerSessionPerUserCategory {
+    user_profile: number
+    avg_pages_viewed: number
+}
+
+export interface AverageTimeSpentPerPagePerUserCategory {
+    user_profile: number
+    avg_time_per_user: number
 }
--- metrics/search/related_search_parameter_consecutive.py
+++ metrics/search/related_search_parameter_consecutive.py
@@ -16,14 +16,6 @@ def calculate_related_search_parameters(logs_df):
     filtering unnecessary results, and calculating the sessions.
     It then computes the Jaccard similarity for the search
     terms within each session and stores the results in a DataFrame.
-
-    Args:
-        logs_df (DataFrame): The input DataFrame containing the raw search
-        logs.
-
-    Returns:
-        results_df (DataFrame): A Polars DataFrame with the session ID, search
-        pairs, and their respective Jaccard similarity index.
     """
     map_requests_df = convert_timestamp(logs_df)
 
@@ -73,15 +65,6 @@ def extract_search_params(map_requests_df):
     Additionally, it filters out non-successful HTTP responses,
     autocompleted requests based on their response size, and requests
     with short response times.
-
-    Args:
-        map_requests_df (DataFrame): The input DataFrame
-        containing session logs.
-
-    Returns:
-        grouped_sessions (DataFrame): A grouped DataFrame containing
-        unique sessions and
-        their respective search parameters and times.
     """
     map_requests_df = map_requests_df.with_columns(
         pl.when(pl.col("request_url").str.contains(r"q="))
@@ -117,27 +100,14 @@ def calculate_jaccard_similarity(
     the search terms. The results are returned in a list containing
     the session ID, the search
     pairs, and their corresponding Jaccard similarity score.
-
-    Args:
-        search_list (list): A list of search terms.
-        search_time (list): A list of timestamps for the search terms.
-        session_id (str): The unique session ID.
-        time_threshold (int): The maximum allowed time difference between
-        searches (in seconds).
-        max_distance (int): The maximum allowed edit distance for terms to be
-        considered autocompleted.
-        max_comb_size (int): The maximum size of the search term combinations.
-
-    Returns:
-        session_results (list): A list of dictionaries with session ID, search
-        pairs, and
-                                Jaccard similarity scores.
     """
     session_results = []
     for size in range(2, min(max_comb_size + 1, len(search_list) + 1)):
         for combo in combinations(range(len(search_list)), size):
-            terms = [search_list[i] for i in combo]
-            times = [search_time[i] for i in combo]
+            sorted_combo = sorted(combo, key=lambda i: search_time[i])
+
+            terms = [search_list[i] for i in sorted_combo]
+            times = [search_time[i] for i in sorted_combo]
 
             if any(is_autocomplete(terms[i], terms[j], max_distance=max_distance)
                    for i, j in combinations(range(len(terms)), 2)):
@@ -150,7 +120,7 @@ def calculate_jaccard_similarity(
             if any(diff > time_threshold for diff in time_diffs):
                 continue
 
-            list_combo = [set(term.split()) for term in terms]
+            list_combo = [set(term.split('+')) for term in terms]
             intersection = list_combo[0].intersection(*list_combo[1:])
             union = list_combo[0].union(*list_combo[1:])
 
@@ -174,16 +144,6 @@ def is_autocomplete(term1, term2, max_distance=1):
     to be considered
     an autocompletion. It also considers the length difference
     between the terms.
-
-    Args:
-        term1 (str): The first search term.
-        term2 (str): The second search term.
-        max_distance (int): The maximum allowed edit distance
-        to consider a term
-        autocompleted.
-
-    Returns:
-        bool: True if the terms are considered autocompleted, otherwise False.
     """
     distance = levenshtein_distance(term1, term2)
     length_difference = abs(len(term1) - len(term2))
@@ -198,45 +158,20 @@ def levenshtein_distance(a, b):
     The Levenshtein distance measures the minimum number of
     single-character edits required
     to transform one string into another.
-
-    Args:
-        a (str): The first string.
-        b (str): The second string.
-
-    Returns:
-        int: The Levenshtein distance between the two strings.
     """
     return len(list(difflib.ndiff(a, b))) // 2
 
 
-def convert_to_datetime(t):
+def convert_to_datetime(date):
     """
     Convert a string to a datetime object if necessary.
-
-    Args:
-        t (str or datetime): A string representing a timestamp or
-        an existing datetime object.
-
-    Returns:
-        datetime: A datetime object representing the input time.
     """
-    if isinstance(t, str):
-        try:
-            return datetime.strptime(t, '%Y-%m-%d %H:%M:%S.%f')
-        except ValueError:
-            return datetime.strptime(t, '%Y-%m-%d %H:%M:%S')
-    return t
+    return datetime.strptime(date, "%Y-%m-%d %H:%M:%S.%f")
 
 
 def filter_search_terms(search_list):
     """
     Filter search terms that have more than two characters.
-
-    Args:
-        search_list (list): A list of search terms.
-
-    Returns:
-        list: A filtered list of search terms with more than 2 characters.
     """
     return [term for term in search_list if len(term) > 2]
 
@@ -244,13 +179,6 @@ def filter_search_terms(search_list):
 def convert_timestamp(logs_df):
     """
     Convert the 'timestamp' column to datetime format, including microseconds.
-
-    Args:
-        logs_df (DataFrame): The input DataFrame containing raw logs.
-
-    Returns:
-        DataFrame: The DataFrame with the 'timestamp' column
-        converted to datetime.
     """
     logs_df = logs_df.with_columns(
         pl.col("timestamp").str.replace(" UTC", "").alias("search_time")
